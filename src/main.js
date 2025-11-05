3b68706d9c7c26d40f4be540e4ef68b50b8861eb
Yuliia <semockoula@gmail.com>
2025-11-05 20:38:40 +0200

    add order-modal and styles

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
 import 'modern-normalize/modern-normalize.css';
 import './css/styles.css';
 
+import './js/About-Us-Section';
+import './js/FAQ-Section';
+import './js/Feedback-Section';
 
-import "./js/About-Us-Section"
-import "./js/FAQ-Section"
-import "./js/Feedback-Section"
-
-import "./js/Furniture-Details-Modal"
-import "./js/Furniture-List-Section"
-import "./js/Hero-Section"
-import "./js/Order-Modal"
-import "./js/header"
-import "./js/Footer"
-import "./js/products-api"
-import "./js/render-function"
+import './js/Furniture-Details-Modal';
+import './js/Furniture-List-Section';
+import './js/Hero-Section';
+import './js/order-modal';
+import './js/header';
+import './js/Footer';
+import './js/products-api';
+import './js/render-function';
