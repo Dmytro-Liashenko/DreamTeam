54c3ee005c0682dd26be59bb6ba284a586f20ecc
Daniil Rybchynskyi <rybchynskyidaniil@gmail.com>
2025-11-06 22:00:04 +0000

    a

--- src/js/FAQ-Section.js
+++ src/js/FAQ-Section.js
@@ -1,7 +1,7 @@
 import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
+// import 'accordion-js/dist/accordion.min.css';
 import '../css/FAQ-Section.css'
 new Accordion('.accordion-container', {
-  duration: 300,
+  duration: 250,
     showMultiple: false,
 });
